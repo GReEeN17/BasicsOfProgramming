40d184a2bd7482881ee20a4b7c0c0e98f740bbea
Aleksandr <azelen04@mail.ru>
2022-12-06 12:26:15 +0300

    Added reading from .mp3 file

--- LAB_13/MP3ID3V2.c
+++ LAB_13/MP3ID3V2.c
@@ -38,6 +38,56 @@ struct MP3File {
 };
 #pragma pop;
 
+unsigned int getSize(unsigned sizeBytes) {
+    return ((sizeBytes >> 24) & 0x000000ff) | ((sizeBytes >> 8) & 0x0000ff00) | ((sizeBytes << 8) & 0x00ff0000) | ((sizeBytes << 24) & 0xff000000);
+}
+
+short isFrameCorrect(struct FrameHeader* frameHeader) {
+    if (frameHeader->frameID[0] == 0 && frameHeader->frameID[1] == 0 && frameHeader->frameID[2] == 0 && frameHeader->frameID[3] == 0) {
+        return 0;
+    }
+    if (frameHeader->sizeBytes == 0) {
+        return 0;
+    }
+    return 1;
+}
+
+struct MP3File* readInf(char* fileName) {
+    FILE* file = fopen(fileName, "rb");
+    if (file == NULL) {
+        return NULL;
+    }
+    struct MP3File* mp3 = (struct MP3File*) malloc(sizeof(struct MP3File));
+    mp3->fileName = fileName;
+    mp3->header = (struct ID3V2Header*) malloc(sizeof(struct ID3V2Header));
+    fread(mp3->header, sizeof(struct ID3V2Header), 1, file);
+    if (mp3->header->flags >> 6 != 0) {
+        mp3->extendedHeader = (struct ID3V2ExtendedHeader*) malloc(sizeof(struct ID3V2ExtendedHeader));
+        fread(mp3->extendedHeader, sizeof(struct ID3V2ExtendedHeader), 1, file);
+    } else {
+        mp3->extendedHeader = NULL;
+    }
+    unsigned headerSize = getSize(mp3->header->sizeBytes);
+    mp3->frameAmount = 0;
+    mp3->frames = NULL;
+    while (ftell(file) < headerSize + sizeof(struct ID3V2Header)) {
+        struct Frame* currentFrame = (struct Frame*) malloc(sizeof(struct Frame));
+        currentFrame->frameHeader = (struct FrameHeader*) malloc(sizeof(struct FrameHeader));
+        fread(currentFrame->frameHeader, sizeof(struct FrameHeader), 1, file);
+        if (!isFrameCorrect(currentFrame->frameHeader)) break;
+        unsigned frameDataSize = getSize(currentFrame->frameHeader->sizeBytes);
+        currentFrame->frameData = (char*) malloc(frameDataSize * sizeof(char));
+        mp3->frameAmount++;
+        mp3->frames = realloc(mp3->frames, mp3->frameAmount * sizeof(struct Frame));
+        mp3->frames[mp3->frameAmount - 1] = currentFrame;
+    }
+    mp3->dataSize = headerSize + sizeof(struct ID3V2Header) - ftell(file);
+    mp3->data = (char*) malloc(mp3->dataSize * sizeof(char));
+    fread(mp3->data, sizeof(char), mp3->dataSize, file);
+    fclose(file);
+    return mp3;
+}
+
 
 
 int main(int argc, char* argv[]) {
