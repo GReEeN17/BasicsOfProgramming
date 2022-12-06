9579ff09975577b36b4d3fb1ed1f511572db6b6d
Aleksandr <azelen04@mail.ru>
2022-12-06 23:16:57 +0300

    Added reading from file and freeing memory

--- LAB_14/game_of_life.c
+++ LAB_14/game_of_life.c
@@ -26,10 +26,39 @@ struct BitMapInfoHeader {
     unsigned colorsImportant;
 };
 
-struct BMPfile {
+struct BMPFile {
     struct BitMapFileHeader* bitMapFileHeader;
     struct BitMapInfoHeader* bitMapInfoHeader;
+    unsigned char* data;
 };
 
 #pragma pack(pop)
+
+void readBMPRowByRow(FILE* file, struct BMPFile* bmp) {
+
+}
+
+struct BMPFile* readBMP (char* fileName) {
+    FILE* file = fopen(fileName, "rb");
+    if (file == NULL) {
+        printf("Can't load file");
+        exit(0);
+    }
+    struct BMPFile* bmp = (struct BMPFile*) malloc(sizeof(struct BMPFile));
+    fread(bmp->bitMapFileHeader, sizeof(struct BitMapFileHeader), 1, file);
+    fread(bmp->bitMapInfoHeader, sizeof(struct BitMapInfoHeader), 1, file);
+    int data_size = bmp->bitMapInfoHeader->width * bmp->bitMapInfoHeader->height * bmp->bitMapInfoHeader->bitCount / 8;
+    bmp->data = (unsigned char*) malloc(data_size);
+    readBMPRowByRow(file, bmp);
+    fclose(file);
+    return bmp;
+}
+
+void freeBMPFile (struct BMPFile* bmp) {
+    if (bmp){
+        free(bmp);
+    }
+}
+
+
 
