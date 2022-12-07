73e0e26ddcd584bbb91f2eeab023132a6c585e74
Aleksandr <azelen04@mail.ru>
2022-12-07 10:28:08 +0300

    Added write to bmp file and two variants of reading of data(image and color pallet)

--- LAB_14/game_of_life.c
+++ LAB_14/game_of_life.c
@@ -34,9 +34,30 @@ struct BMPFile {
 
 #pragma pack(pop)
 
-void readBMPRowByRow(FILE* file, struct BMPFile* bmp) {
-
-}
+/*void readBMPRowByRow(FILE* file, struct BMPFile* bmp) {
+    int bytesPerPixel = bmp->bitMapInfoHeader->bitCount;
+    int rowSize = bytesPerPixel * bmp->bitMapInfoHeader->width;
+    int rowPadding = (4 - (rowSize % 4)) % 4;
+    int rowsWritten = 0;
+    unsigned char* row = (unsigned char*) malloc(rowSize + rowPadding);
+    unsigned char* p = &bmp->data[(bmp->bitMapInfoHeader->height - 1) * rowSize];
+    fseek(file, bmp->bitMapFileHeader->offsetBits, SEEK_SET);
+    while (rowsWritten < bmp->bitMapInfoHeader->height) {
+        fread(row, rowSize + rowPadding, 1, file);
+        if (bytesPerPixel == 3) {
+            for (int i = 0; i < rowSize; i += bytesPerPixel) {
+                *p = row[i + 2]; p++;
+                *p = row[i + 1]; p++;
+                *p = row[i]; p++;
+            }
+        } else {
+            printf("Error: wrong number of bytes per pixel");
+            exit(0);
+        }
+        rowsWritten++;
+        p = p - 2 * rowSize;
+    }
+}*/
 
 struct BMPFile* readBMP (char* fileName) {
     FILE* file = fopen(fileName, "rb");
@@ -47,11 +68,21 @@ struct BMPFile* readBMP (char* fileName) {
     struct BMPFile* bmp = (struct BMPFile*) malloc(sizeof(struct BMPFile));
     fread(bmp->bitMapFileHeader, sizeof(struct BitMapFileHeader), 1, file);
     fread(bmp->bitMapInfoHeader, sizeof(struct BitMapInfoHeader), 1, file);
-    int data_size = bmp->bitMapInfoHeader->width * bmp->bitMapInfoHeader->height * bmp->bitMapInfoHeader->bitCount / 8;
+    /*int data_size = bmp->bitMapInfoHeader->width * bmp->bitMapInfoHeader->height * bmp->bitMapInfoHeader->bitCount / 8;
     bmp->data = (unsigned char*) malloc(data_size);
-    readBMPRowByRow(file, bmp);
+    readBMPRowByRow(file, bmp);*/
+    bmp->data = (unsigned char*) malloc(bmp->bitMapInfoHeader->size);
+    fread(bmp->data, bmp->bitMapInfoHeader->size, 1, file);
     fclose(file);
     return bmp;
+}
+
+void writeBMP(struct BMPFile* bmp) {
+    FILE* file = fopen("out.bmp", "r+b");
+    fwrite(bmp->bitMapFileHeader, sizeof(struct BitMapFileHeader), 1, file);
+    fwrite(bmp->bitMapInfoHeader, sizeof(struct BitMapInfoHeader), 1, file);
+    fwrite(bmp->data, bmp->bitMapInfoHeader->size, 1, file);
+    fclose(file);
 }
 
 void freeBMPFile (struct BMPFile* bmp) {
